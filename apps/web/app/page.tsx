88617466e9c83e4aeba0d73bff23e497d9871caa
Kiran Khadka <kiran.khadka.dev@gmail.com>
2026-06-28 14:41:49 +1000

    Added some changes

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -2,17 +2,13 @@ import Image, { type ImageProps } from "next/image";
 import { Button } from "@repo/ui/button";
 import styles from "./page.module.css";
 
-import { prisma } from "@repo/db";
+import {prisma}  from "@repo/db"
+
+
 export default async function Home() {
-const user =await prisma.user.findFirst()  
-
-console.log(user)
-
   return (
     <div>
-      <h2>Welcome to the Programming World </h2>
-      <h2>Username: {user?.username}</h2>
-      <p>Password: {user?.password}</p>
+      <h2>Welcome to the programming World </h2>
     </div>
   );
 }
--- packages/prisma/src/index.js
+++ packages/prisma/src/index.js
@@ -1,10 +1,12 @@
 import "dotenv/config";
+
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "../generated/prisma/client.js";
 
 const connectionString = `postgresql://neondb_owner:npg_Z1rJxBg2OsRm@ep-noisy-dawn-ade2qzst-pooler.c-2.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require`;
+// const connectionString = process.env.DATABASE_URL;
+console.log(`Connection String : ${connectionString}`);
 
-
-console.log("connection string",connectionString)
+console.log("connection string", connectionString);
 const adapter = new PrismaPg({ connectionString });
 export const prisma = new PrismaClient({ adapter });
